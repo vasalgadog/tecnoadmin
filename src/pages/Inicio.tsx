dba92eea2d89075dbfdd01c5213abe02e487c184
Valentin Salgado <valentin.salgadog@gmail.com>
2026-04-17 14:42:16 -0400

    style: swap dashboard forms and lock to two columns for desktop sizes

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -5,16 +5,16 @@ export default function Inicio() {
   return (
     <>
       <div className="h-full overflow-y-auto p-6 md:p-8 space-y-6">
-        <div className="grid grid-cols-1 xl:grid-cols-12 gap-6 items-start max-w-[1440px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start max-w-[1440px] mx-auto">
           
-          {/* Panel 1: Bread Registration */}
-          <section className="xl:col-span-5 space-y-6 flex-1">
-             <BreadRegistration />
+          {/* Panel 1: Order Registration (Left) */}
+          <section className="lg:col-span-7 flex-1">
+             <OrderRegistration />
           </section>
 
-          {/* Panel 2: Order Registration */}
-          <section className="xl:col-span-7 flex-1">
-             <OrderRegistration />
+          {/* Panel 2: Bread Registration (Right) */}
+          <section className="lg:col-span-5 space-y-6 flex-1">
+             <BreadRegistration />
           </section>
           
         </div>
